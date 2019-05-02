70f89c041030eab08b42c3d98c058d8b9c588c93
Jikyung Colin Kim <colinjkim1997@gmail.com>
2019-05-02 07:08:33 -0400

    added project and started jQuery

--- Projects/rgbguess/rgb.js
+++ Projects/rgbguess/rgb.js
@@ -6,19 +6,52 @@ var goaldisplay = document.getElementById("goal");
 var message = document.getElementById("message");
 var resetButton = document.getElementById("reset");
 var h1 = document.querySelector("h1");
+var easyBtn = document.getElementById("easy");
+var hardBtn = document.getElementById("hard");
+var numofsq=6;
 start();
+goaldisplay.textContent=goal;
 
 resetButton.addEventListener("click",function(){
     resetButton.textContent="New Color";
-    colors=getRandomCol(6);
+    colors=getRandomCol(numofsq);
     goal=pickColor();
     goaldisplay.textContent=goal;
     message.textContent="";
-    h1.style.backgroundColor="#232323";
+    h1.style.backgroundColor="steelblue";
     start();
 });
 
-goaldisplay.textContent=goal;
+easyBtn.addEventListener("click",function(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    numofsq=3;
+    colors=getRandomCol(numofsq);
+    goal=pickColor();
+    goaldisplay.textContent=goal;
+    for(var i=0;i<squares.length;i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor=colors[i];
+        }
+        else{
+            squares[i].style.display="none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click",function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numofsq=6;
+    colors=getRandomCol(numofsq);
+    goal=pickColor();
+    goaldisplay.textContent=goal;
+    for(var i=0;i<squares.length;i++){
+        squares[i].style.backgroundColor=colors[i];
+        squares[i].style.display="block";
+    }
+});
+
 
 function start(){
     for(var i =0; i<squares.length;i++){
