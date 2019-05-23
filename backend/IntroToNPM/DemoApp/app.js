bd9b67220f0e2f619efb4c1e209e9e460a0c81da
Jikyung Colin Kim <colinjkim1997@gmail.com>
2019-05-22 20:02:01 -0400

    testing npm packages

---
+++ backend/IntroToNPM/DemoApp/app.js
@@ -0,0 +1,8 @@
+console.log("FROM APP.JS");
+
+var cat = require("cat-me");
+
+console.log(cat());
+
+var badjokes = require("knock-knock-jokes");
+console.log(badjokes());
---
+++ backend/IntroToNode/echo.js
@@ -0,0 +1,8 @@
+function echo(str, num){
+    for(var i = 0; i<num; i++){
+        console.log(str);
+    }
+}
+echo("echoo!!!", 10)
+echo("print it three times",3)
+
