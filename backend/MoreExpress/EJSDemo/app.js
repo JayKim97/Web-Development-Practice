1adaaa5f5eff8939e62ecdf74da9f6dfd9a9b71f
Jikyung Colin Kim <colinjkim1997@gmail.com>
2019-05-23 04:59:26 -0400

    ejs header footer and scripting included

--- backend/MoreExpress/EJSDemo/app.js
+++ backend/MoreExpress/EJSDemo/app.js
@@ -1,12 +1,26 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/",function(req,res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing",function(req,res){
     var thing = req.params.thing;
-    res.render("love.ejs",{thing:thing});
+    res.render("love",{thing:thing});
+});
+
+app.get("/posts", function(req,res){
+    var posts = [
+        {title: "love soyeon", author: "abd"},
+        {title: "Jeonsoyeon", author: "gidle"},
+        {title: "(G)I-dle", author: "neverland"}
+    ];
+
+    res.render("posts",{posts: posts});
 });
 
 app.listen(3000, function(){
