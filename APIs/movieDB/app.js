b4d1f16d868625ae35d279afcaf71e36b27b3d9c
Jikyung Colin Kim <colinjkim1997@gmail.com>
2019-05-27 01:04:24 -0400

    finished simple movie db access app

--- APIs/movieDB/app.js
+++ APIs/movieDB/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const request = require("request");
 const app = express();
+app.set("view engine", "ejs");
+
+app.get("/",(req,res)=>{
+    res.render("search");
+});
 
 app.get("/results", (req, res)=> {
-    request("http://www.omdbapi.com/?s=star&apikey=thewdb", (error, response,body) => {
+    var search = req.query.search;
+    var url = "http://www.omdbapi.com/?s="+search+"&apikey=thewdb"
+    request(url, (error, response,body) => {
         if(!error && response.statusCode==200){
-            const results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]);
+            const data = JSON.parse(body);
+            res.render("results",{data:data});
         }
     });
     
