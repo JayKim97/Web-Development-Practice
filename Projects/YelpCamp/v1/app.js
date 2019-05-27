96d9031c518e809ba5df18a02ff9632db0767705
Jikyung Colin Kim <colinjkim1997@gmail.com>
2019-05-27 18:38:56 -0400

    finished v1 of the yelpcamp

--- Projects/YelpCamp/v1/app.js
+++ Projects/YelpCamp/v1/app.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+
+var campgrounds=[
+    {name: "Salmon Creek", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"},
+    {name: "Hopeful Future", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"},
+    {name: "Depressing Past", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"},
+    {name: "Salmon Creek", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"},
+    {name: "Hopeful Future", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"},
+    {name: "Depressing Past", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"},
+    {name: "Salmon Creek", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"},
+    {name: "Hopeful Future", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"},
+    {name: "Depressing Past", image: "https://mikeputnamphoto.files.wordpress.com/2008/08/lower-meadow-campsite.jpg"}
+];
 
 app.get("/",(req, res)=>{
     res.render("landing");
 });
 
-app.get("/campgorunds", (req,res)=>{
-    
+app.get("/campgrounds", (req,res)=>{
+    res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+app.post("/campgrounds",(req,res)=>{
+   var name = req.body.name;
+   var image = req.body.image;
+   var newCampground = {name: name, image: image};
+   campgrounds.push(newCampground);
+   res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new",(req,res)=>{
+    res.render("new.ejs");
 });
 
 app.listen(3000,()=>{
