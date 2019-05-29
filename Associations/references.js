7ec3fbffce8aad9b62cf7ca4f9a8246a8fa98d41
Jikyung Colin Kim <colinjkim1997@gmail.com>
2019-05-29 01:08:08 -0400

    sample with using reference data model:

--- Associations/references.js
+++ Associations/references.js
@@ -12,8 +12,48 @@ const Post = mongoose.model("post", postSchema);
 const userSchema = new mongoose.Schema({
     email: String,
     name: String,
-    posts: [postSchema]
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post"
+    }]
 });
 const User = mongoose.model("User", userSchema);
 
+// User.create({
+//     email: "bob@gmail.com",
+//     name: "Bob Gmail"
+// });
 
+//posting and adding reference to user
+// Post.create({
+//    title: "How to the best burger pt. 3",
+//    content:"finished it" 
+// },(err,post)=>{
+//     User.findOne({email: "bob@gmail.com"},(err, foundUser)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             foundUser.posts.push(post);
+//             foundUser.save((err, data)=>{
+//                 if(err){
+//                     console.log(err);
+//                 }
+//                 else{
+//                     console.log(data);
+//                 }
+//             });
+//         }
+//     });
+// });
+
+//Find user
+// Find all posts for that user
+User.findOne({email: "bob@gmail.com"}).populate("posts").exec((err,user)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(user);  
+    }
+});
